9439414825f00111e49231f8d8edc6370392a6a4
bachalimed <69384160+bachalimed@users.noreply.github.com>
2024-06-21 14:17:14 +0100

    cqlled props from children sidemenu and arranged icons

--- src/Components/Shared/NavbarSidebarUp.js
+++ src/Components/Shared/NavbarSidebarUp.js
@@ -1,70 +1,233 @@
 import React from 'react'
-//import { Link } from 'react-router-dom';
-import { RiDashboard2Line } from "react-icons/ri";
-//import { TbReport } from "react-icons/tb";
-//import { FaChevronDown } from "react-icons/fa";
-//import { PiStudentBold } from "react-icons/pi";
-//import { FaSackDollar } from "react-icons/fa6";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { VscDashboard } from "react-icons/vsc";
+import { PiStudent, PiStudentBold } from "react-icons/pi";
+import { BsChevronDown } from "react-icons/bs";
+import { GrSchedules } from "react-icons/gr";
+import { LuCircleDollarSign } from "react-icons/lu";
+import { GiHumanPyramid } from "react-icons/gi";
+import { FaMailBulk } from "react-icons/fa";
+import { SiWebmoney } from "react-icons/si";
+import { SlSettings } from "react-icons/sl";
+import { BiHome } from "react-icons/bi";
 
-
-
-
-
-
-
-const NavbarSidebarUp = () => {
-
-	const Menus=[
-		{title:"Dashboard",
-		icon: "RiDashboard2Line",
-		path:"/Dashboard",
-		spacing:true
-		 },
-		 {title:"Student Affairs",
+const Menus=[
+	{title:"Home",
+	icon: <BiHome/>,
+	path:"/",
+	spaced:false
+	 },
+	{title:"Dashboard",
+	icon: <VscDashboard/>,
+	path:"/",
+	spaced:false
+	 },
+	 
+	{title:"Students",
+	icon: <PiStudent/>,
+	path:"",
+	submenu:true,
+	submenuItems:
+		[{title:"Students & Parents",
+			icon: <PiStudent/>,
+			path:"/",
+			spaced:false
+			},
+			{title:"Admissions",
 			icon: "",
-			path:"/StudentAffairs",
-			submenu:true,
-			SubmenuItems:
-			[{title:"Students & Parents",
-				icon: "RiDashboard2Line",
-				path:"/Student",
-				spacing:true
-				 },
-				 {title:"Admissions",
-					icon: "RiDashboard2Line",
-					path:"",
-					spacing:true
-					 },
-					 {title:"Enrolments",
-						icon: "RiDashboard2Line",
-						path:"",
-						spacing:true
-						 }
-
+			path:"",
+			spaced:false
+			},
+			{title:"Enrolments",
+			icon: "",
+			path:"",
+			spaced:false
+			}
+		]
+	},
+	{title:"Academics",
+		icon: <GrSchedules/>,
+		path:"",
+		submenu:true,
+		submenuItems:
+		[{title:"School Plan",
+			icon: "",
+			path:"/",
+			spaced:false
+			},
+			{title:"Nursery Plan",
+			icon: "",
+			path:"",
+			spaced:false
+			},
+			{title:"Collection",
+				icon: "",
+				path:"",
+				spaced:false
+			}
 			]
-			 },
-			 {title:"Finances",
-				icon: "FaSackDollar",
-				path:"/Finances"
-				 }
-	]
+	},
+	{title:"Finances",
+	icon: <LuCircleDollarSign/>,
+	path:"",
+	spaced:false,
+	submenu:true,
+	submenuItems:
+		[{title:"Invoices",
+			icon: "",
+			path:"/",
+			spaced:false
+			},
+			{title:"Payments",
+			icon: "",
+			path:"",
+			spaced:false
+			},
+			{title:"Expenses",
+				icon: "",
+				path:"",
+				spaced:false
+			}
+			]
 
+		},
 	
-
-  return (
-	<div className=''>
+	{title:"HR",
+	icon: <GiHumanPyramid/>,
+	path:"",
+	spaced:false,
+	submenu:true,
+	submenuItems:
+		[{title:"Staff",
+			icon: "",
+			path:"/",
+			spaced:false
+			},
+			{title:"Payroll",
+			icon: "",
+			path:"",
+			spaced:false
+			},
+			{title:"Leave",
+				icon: "",
+				path:"",
+				spaced:false
+			}
+			]
+		},
+	{title:"Desk",
+	icon: <FaMailBulk/>,
+	path:"",
+	spaced:false,
+	submenu:true,
+	submenuItems:
+		[{title:"Inquiries",
+			icon: "",
+			path:"/",
+			spaced:false
+			},
+			{title:"Tasks",
+			icon: "",
+			path:"",
+			spaced:false
+			},
+			{title:"Communication",
+				icon: "",
+				path:"",
+				spaced:false
 			
-	<ul className='pt-2'>
-		{Menus.map((menu, index)=>(
-			<>
-			<li key={index} className='text-grey-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2'>
-				<span className='text-2xl block float-left'> <RiDashboard2Line/></span>
-				
-			</li>
-			</>
-		))}
-	</ul>
-	</div>)
-}
+			},
+			{title:"Mailing",
+				icon: "",
+				path:"",
+				spaced:false
+			}
+			]
+	},
+	{title:"CMS",
+	icon: <SiWebmoney/>,
+	path:"",
+	spaced:false
+	},
+	{title:"Settings",
+		icon: <SlSettings/>,
+		path:"/StudentAffairs",
+		submenu:true,
+		submenuItems:
+		[{title:"Dashboard",
+			icon: <PiStudentBold/>,
+			path:"/Student",
+			
+			 },
+			 {title:"Students",
+				icon: "",
+				path:"",
+				spaced:false
+				 },
+			 
+				 {title:"Academics",
+					icon: "",
+					path:"",
+					spaced:false
+					 },
+			 
+				 {title:"Finances",
+					icon: "",
+					path:"",
+					spaced:false
+					 },
+			 
+				 {title:"HR",
+					icon: "",
+					path:"",
+					spaced:false
+					 },
+			 
+				 {title:"Desk",
+					icon: "",
+					path:"",
+					spaced:false
+					 },
+			 
+				 {title:"CMS",
+					icon: "",
+					path:"",
+					spaced:false
+					 }
+		]
+		 }
+	
+]
 
-export default NavbarSidebarUp ();
+const NavbarSidebarUp = (props) => {
+	const [submenuOpen, setSubmenuOpen] = useState(false);
+	return (
+		<div   >
+		<ul className='pt-2'> {Menus.map((menu, index)=>(
+			<>
+				<li key={index} className={`text-grey-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-sky-700 rounded-md ${menu.spaced ? "mt-8" : "mt-0" } `}>
+				<span className='text-2xl block float-left'> {menu.icon? menu.icon:<VscDashboard/>}</span>
+				<span className={`text-base font-medium flex-1 ${!props.open && "hidden"} duration-200`}> {menu.title}</span>
+				{menu.submenu&& props.open &&(
+					<BsChevronDown className={`${submenuOpen && "rotate-180"}`} onClick={()=>
+						setSubmenuOpen(!submenuOpen)}/>
+				)}
+				</li>
+				{menu.submenu&& submenuOpen && props.open && (
+					<ul>
+						{menu.submenuItems.map((submenuItem, index)=>(
+							<li key={index} className='text-grey-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5  hover:bg-sky-700 rounded-md'>
+								{submenuItem.title}
+
+							</li>
+						))	}
+					</ul>
+				)}
+			</>
+			))}
+		</ul>
+	</div>)
+  }
+
+export default NavbarSidebarUp;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,114 +2,34 @@ import React from 'react'
 import { useState } from 'react'
 
 
+import NavbarSidebarUp from './Shared/NavbarSidebarUp'
 import NavbarSidebarDown from './Shared/NavbarSidebarDown'
 import logo from './../Data/logo.jpg'
-import { RiDashboard2Line } from "react-icons/ri";
+
 import { CgPushChevronLeftR } from "react-icons/cg";
-import { BsChevronDown } from "react-icons/bs";
-import { TbReport } from "react-icons/tb";
 
-import { PiStudentBold } from "react-icons/pi";
-import { FaSackDollar } from "react-icons/fa6";
-const Menus=[
-	{title:"Dashboard",
-	icon: <RiDashboard2Line/>,
-	path:"/Dashboard",
-	spaced:true
-	 },
-	 
-	 {title:"Students",
-		icon: <PiStudentBold/>,
-		path:"/StudentAffairs",
-		submenu:true,
-		submenuItems:
-		[{title:"Students & Parents",
-			icon: <PiStudentBold/>,
-			path:"/Student",
-			
-			 },
-			 {title:"Admissions",
-				icon: "",
-				path:"",
-				spaced:false
-				 },
-			 
-				 {title:"Enrolments",
-					icon: "",
-					path:"",
-					spaced:true
-					 }
 
-		]
-		 },
-		 {title:"Academics",
-			icon: "",
-			path:"",
-			spaced:false
-			 },
-		 {title:"CMS",
-			icon: "",
-			path:"/",
-			spaced:true
-			 },
-		 {title:"Desk",
-			icon: <FaSackDollar/>,
-			path:"/",
-			spaced:true
-			 }
-]
 
 
 const Sidebar = () => {
 	//to set the open close of side bar
 	const [open, setOpen]= useState(true);
-	const [submenuOpen, setSubmenuOpen] = useState(false);
+	
 	const company ={label:"First Steps", type:"Nursery"};
 
 	
   return (
     
-	<div className={`bg-neutral-900 ${open?"w-56":"w-20" } p-3 flex flex-col text-white h-full duration-300 relative`}> 
+	<div className={`bg-gray-900 ${open?"w-56":"w-20" } p-3 flex flex-col text-white h-full duration-300 relative`}> 
 		<div className='inline-flex items-center '>
 			<img  src = {logo} className='h-14 w-14 rounded block float-left mr-2 mt-4 ' alt='logo image' />
 			<span className={`origin-left duration-300 ${!open&&"scale-0"  }`}>{company.label} <br/>{company.type}</span>
 		</div>
 		<CgPushChevronLeftR className={` text-black text-3xl  ${!open&&"rotate-180" } absolute -right-9 top-7 cursor-pointer`} onClick={()=>setOpen(!open)}/>
-
-		<div className='flex-1'  >
-			<ul className='pt-2'> {Menus.map((menu, index)=>(
-				<>
-					<li key={index} className={`text-grey-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.spaced ? "mt-8" : "mt-1" } `}>
-					<span className='text-2xl block float-left'> {menu.icon? menu.icon:<RiDashboard2Line/>}</span>
-					<span className={`text-base font-medium flex-1 ${!open && "hidden"} duration-200`}> {menu.title}</span>
-					{menu.submenu&& open &&(
-						<BsChevronDown className={`${submenuOpen && "rotate-180"}`} onClick={()=>
-							setSubmenuOpen(!submenuOpen)}/>
-					)}
-					</li>
-					{menu.submenu&& submenuOpen && open && (
-						<ul>
-							{menu.submenuItems.map((submenuItem, index)=>(
-								<li key={index} className='text-grey-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-light-white rounded-md'>
-									{submenuItem.title}
-
-								</li>
-							))	}
-						</ul>
-					)}
-				</>
-				))}
-			</ul>
-		</div>
-
-		<div><NavbarSidebarDown/></div>
-
-
-
-
-
-    	</div>
-  )
-}
+		
+		<div className='flex-1'><NavbarSidebarUp open={open}/></div>
+		<div><NavbarSidebarDown open={open}/></div>
+	</div>
+  )}
 
 export default Sidebar;
