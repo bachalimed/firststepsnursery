1b5f5ff797820ec608c686e109433a67a6335059
bachalimed <69384160+bachalimed@users.noreply.github.com>
2024-06-23 00:23:01 +0100

    added icons to submenu in sidebar

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { HiOutlineSearch } from 'react-icons/hi';
 
 
 const Footer = () => {
   return (
-    <div className='flex justify-between h-24 items-center'>
+    <div className='flex justify-between h-24 items-center border-b border-gray-200'>
       This is the footer component
-      <div>
+      <div className='relative'>
+      <HiOutlineSearch fontSize={20} className='text-gray-400 absolute top-1/2 -translate-y-1/2 left-3'/>
         <input
         type='text'
         placeholder='search'
-        className='text-sm focus:outline-none active:outline-none h-10 w-[24rem] border border-gray-200'    />
+        className='text-sm focus:outline-none active:outline-none h-10 w-[24rem] border border-gray-300 rounded-md px-4 pl-11 pr-4'   />
       </div>
       
     </div>
--- src/Components/Shared/NavbarHeader.js
+++ src/Components/Shared/NavbarHeader.js
@@ -1,14 +1,26 @@
 //import { Link } from "react-router-dom";
 
+import { HiOutlineBell } from "react-icons/hi2";
+import { HiOutlineChatAlt } from "react-icons/hi";
+import { LuUserCircle2 } from "react-icons/lu";
+
 const NavbarHeader = () => {
   return (
-    <div className='flex'>
+    <div className='flex items-center gap-2 mr-2'>
      
         
-            <div>Notifications</div>
+            <div className="">
+              <HiOutlineChatAlt fontSize={24}/>
+            </div>
+            <div>
+              <HiOutlineBell fontSize={24}/>
+            </div>
+            <div>
+              <LuUserCircle2 fontSize={24}/>
+            </div>
             
          
-            <div>profile</div>
+            <div>dropdwn</div>
          
                
     </div>
